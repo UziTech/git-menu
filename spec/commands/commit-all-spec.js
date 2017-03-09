8ffc893d5b914e34cf4c047b7a1228b96d90ce37
Tony Brix <Tony@Brix.ninja>
2017-03-09 14:26:20 -0600

    add atom-jasmine2-test-runner

--- spec/commands/commit-all-spec.js
+++ spec/commands/commit-all-spec.js
@@ -1,20 +1,19 @@
 "use babel";
 /* globals atom, describe, it, expect, beforeEach, afterEach, waitsForPromise, runs, spyOn, jasmine */
 
-import commands, { commit } from "../../lib/commands";
-const commitAll = commands["commit-all"];
+import commands from "../../lib/commands";
 import { getFilePath, removeGitRoot, createGitRoot, files } from "../fixtures";
 
 describe("commit-all", function () {
 
-	beforeEach(function () {
-		waitsForPromise(async _ => {
-			await atom.packages.activatePackage("context-git");
+	beforeEach(function (done) {
+		atom.packages.activatePackage("context-git").then(_ => {
 			createGitRoot();
 			this.gitRoot = getFilePath();
 			atom.project.setPaths([this.gitRoot]);
 
 			this.filePaths = getFilePath([files.t1]);
+			done();
 		});
 	});
 
@@ -22,13 +21,11 @@ describe("commit-all", function () {
 		removeGitRoot();
 	});
 
-	it("should call commit with project folders", function () {
-		waitsForPromise(async _ => {
-			spyOn(commit, "command").andCallFake(_ => Promise.reject());
-			try {
-				await commitAll.command(this.filePaths);
-			} catch (ex) {}
-			expect(commit.command.mostRecentCall.args[0]).toEqual(atom.project.getPaths());
+	it("should call commit with project folders", function (done) {
+		spyOn(commands.commit, "command").and.callFake(_ => Promise.reject());
+		commands["commit-all"].command(this.filePaths).then(_ => { throw "Not supposed to be here"; }, _ => {
+			expect(commands.commit.command.calls.mostRecent().args[0]).toEqual(atom.project.getPaths());
+			done();
 		});
 	});
 });
