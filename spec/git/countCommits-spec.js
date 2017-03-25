7aeb68583cc7d66e0c07a2cd3ddf8587bff1eb19
Tony Brix <Tony@Brix.ninja>
2017-03-25 15:27:56 -0500

    test no git folder

--- spec/git/countCommits-spec.js
+++ spec/git/countCommits-spec.js
@@ -12,7 +12,6 @@ describe("git.countCommits", function () {
 		createGitRoot();
 		this.gitRoot = getFilePath();
 		atom.project.setPaths([this.gitRoot]);
-		await gitCmd.cmd(this.gitRoot, ["init"]);
 		this.gitPath = getFilePath(".git");
 	});
 
@@ -20,11 +19,26 @@ describe("git.countCommits", function () {
 		removeGitRoot();
 	});
 
+	// TODO: right now it gets the number of commits of context-git since it is a git folder.
+	// TODO: maybe we need to move the gitRoot outside of the context-git folder?
+	//
+	// it("should return error if not git", async function () {
+	// 	let error = false;
+	// 	try {
+	// 		await gitCmd.countCommits(this.gitRoot);
+	// 	} catch (ex) {
+	// 		error = true;
+	// 	}
+	// 	expect(error).toBe(true);
+	// });
+
 	it("should return 0 if no commits", async function () {
+		await gitCmd.cmd(this.gitRoot, ["init"]);
 		expect(await gitCmd.countCommits(this.gitRoot)).toBe(0);
 	});
 
 	it("should be number of commits", async function () {
+		await gitCmd.cmd(this.gitRoot, ["init"]);
 		await gitCmd.cmd(this.gitRoot, ["add", "."]);
 		await gitCmd.cmd(this.gitRoot, ["commit", "-m", "init"]);
 		expect(await gitCmd.countCommits(this.gitRoot)).toBe(1);
