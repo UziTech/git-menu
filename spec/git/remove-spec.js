f1fe2c8b8f35c56a093aad807439fc8cb974cd02
Tony Brix <Tony@Brix.ninja>
2017-03-24 23:04:03 -0500

    add git.remove tests

---
+++ spec/git/remove-spec.js
@@ -0,0 +1,36 @@
+"use babel";
+/* globals atom */
+
+import gitCmd from "../../lib/git-cmd";
+import { getFilePath, removeGitRoot, createGitRoot } from "../mocks";
+import fs from "fs";
+
+describe("git.remove", function () {
+
+	beforeEach(async function () {
+		await atom.packages.activatePackage("context-git");
+		createGitRoot();
+		this.gitRoot = getFilePath();
+		atom.project.setPaths([this.gitRoot]);
+		await gitCmd.cmd(this.gitRoot, ["init"]);
+		this.gitPath = getFilePath(".git");
+	});
+
+	afterEach(function () {
+		removeGitRoot();
+	});
+
+	it("should remove the .git folder", async function () {
+		await gitCmd.remove(this.gitRoot);
+
+		expect(fs.existsSync(this.gitPath)).toBe(false);
+	});
+
+	it("should be idempotent", async function () {
+		await gitCmd.remove(this.gitRoot);
+		await gitCmd.remove(this.gitRoot);
+
+		expect(fs.existsSync(this.gitPath)).toBe(false);
+	});
+
+});
