f6fe5ad2696d3dbf7d5b042e77e3b17e2fa393ee
Tony Brix <Tony@Brix.ninja>
2017-03-27 22:29:55 -0500

    add git tests

--- spec/git/add-spec.js
+++ spec/git/add-spec.js
@@ -6,36 +6,50 @@ import { getFilePath, removeGitRoot, createGitRoot, files } from "../mocks";
 
 describe("git.add", function () {
 
-	beforeEach(async function () {
-		await atom.packages.activatePackage("context-git");
-		createGitRoot();
-		this.gitRoot = getFilePath();
-		atom.project.setPaths([this.gitRoot]);
-		await gitCmd.cmd(this.gitRoot, ["init"]);
-		this.gitPath = getFilePath(".git");
+	describe("unit tests", function () {
+
+		beforeEach(function(){
+			spyOn(gitCmd, "cmd").and.returnValue(Promise.resolve());
+
+			this.files = ["file1", "file2"];
+			this.gitRoot = "root";
+		});
+
+		it("should send ['add', '--', ...files] to cmd", async function(){
+			await gitCmd.add(this.gitRoot, this.files);
+
+			expect(gitCmd.cmd.calls.mostRecent().args[1]).toEqual(["add", "--", ...this.files]);
+		});
+
+		it("should send --verbose to cmd", async function(){
+			await gitCmd.add(this.gitRoot, this.files, true);
+
+			expect(gitCmd.cmd.calls.mostRecent().args[1]).toContain("--verbose");
+		});
 	});
 
-	afterEach(function () {
-		removeGitRoot();
-	});
+	describe("integration tests", function () {
 
-	it("should add a file", async function () {
-		await gitCmd.add(this.gitRoot, getFilePath(files.t1));
-		const status = await gitCmd.cmd(this.gitRoot, ["status", "--short"]);
+		beforeEach(async function () {
+			await atom.packages.activatePackage("context-git");
+			createGitRoot();
+			this.gitRoot = getFilePath();
+			atom.project.setPaths([this.gitRoot]);
+			await gitCmd.cmd(this.gitRoot, ["init"]);
+			this.gitPath = getFilePath(".git");
+		});
 
-		expect(status).toContain(`A  ${files.t1}`);
-	});
+		afterEach(function () {
+			removeGitRoot();
+		});
 
-	it("should return nothing on --quiet", async function () {
-		const result = await gitCmd.add(this.gitRoot, getFilePath(files.t1));
+		it("should add a file", async function () {
+			await gitCmd.add(this.gitRoot, getFilePath(files.t1));
+			const status = await gitCmd.cmd(this.gitRoot, ["status", "--short"]);
 
-		expect(result).toBe("");
-	});
+			expect(status).toContain(`A  ${files.t1}`);
+		});
 
-	it("should return something on verbose", async function () {
-		const result = await gitCmd.add(this.gitRoot, getFilePath(files.t1), true);
-
-		expect(result).not.toBe("");
 	});
 
 });
