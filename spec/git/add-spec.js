6b10fe7864a7270deae243600cbf2440e7b9a06d
Tony Brix <Tony@Brix.ninja>
2017-03-28 08:30:47 -0500

    clean up code

--- spec/git/add-spec.js
+++ spec/git/add-spec.js
@@ -2,30 +2,27 @@
 /* globals atom */
 
 import gitCmd from "../../lib/git-cmd";
-import { getFilePath, removeGitRoot, createGitRoot, files } from "../mocks";
+// import { getFilePath, removeGitRoot, createGitRoot, files } from "../mocks";
 
 describe("git.add", function () {
 
-	describe("unit tests", function () {
+	beforeEach(function () {
+		spyOn(gitCmd, "cmd").and.returnValue(Promise.resolve());
 
-		beforeEach(function(){
-			spyOn(gitCmd, "cmd").and.returnValue(Promise.resolve());
+		this.files = ["file1", "file2"];
+		this.gitRoot = "root";
+	});
 
-			this.files = ["file1", "file2"];
-			this.gitRoot = "root";
-		});
+	it("should send ['add', '--', ...files] to cmd", async function () {
+		await gitCmd.add(this.gitRoot, this.files);
 
-		it("should send ['add', '--', ...files] to cmd", async function(){
-			await gitCmd.add(this.gitRoot, this.files);
+		expect(gitCmd.cmd.calls.mostRecent().args[1]).toEqual(["add", "--", ...this.files]);
+	});
 
-			expect(gitCmd.cmd.calls.mostRecent().args[1]).toEqual(["add", "--", ...this.files]);
-		});
+	it("should send --verbose to cmd", async function () {
+		await gitCmd.add(this.gitRoot, this.files, true);
 
-		it("should send --verbose to cmd", async function(){
-			await gitCmd.add(this.gitRoot, this.files, true);
-
-			expect(gitCmd.cmd.calls.mostRecent().args[1]).toContain("--verbose");
-		});
+		expect(gitCmd.cmd.calls.mostRecent().args[1]).toContain("--verbose");
 	});
 
 	// describe("integration tests", function () {
--- spec/git/init-spec.js
+++ spec/git/init-spec.js
@@ -2,31 +2,27 @@
 /* globals atom */
 
 import gitCmd from "../../lib/git-cmd";
-import { getFilePath, removeGitRoot, createGitRoot } from "../mocks";
-import fs from "fs";
+// import { getFilePath, removeGitRoot, createGitRoot } from "../mocks";
+// import fs from "fs";
 
 describe("git.init", function () {
 
-	describe("unit tests", function () {
+	beforeEach(function () {
+		spyOn(gitCmd, "cmd").and.returnValue(Promise.resolve());
 
-		beforeEach(function(){
-			spyOn(gitCmd, "cmd").and.returnValue(Promise.resolve());
+		this.gitRoot = "root";
+	});
 
-			this.files = ["file1", "file2"];
-			this.gitRoot = "root";
-		});
+	it("should send init commad", async function () {
+		await gitCmd.init(this.gitRoot);
 
-		it("should send init commad", async function(){
-			await gitCmd.init(this.gitRoot);
+		expect(gitCmd.cmd.calls.mostRecent().args[1]).toEqual(["init", "--quiet"]);
+	});
 
-			expect(gitCmd.cmd.calls.mostRecent().args[1]).toEqual(["init", "--quiet"]);
-		});
+	it("should remove --quiet parameter", async function () {
+		await gitCmd.init(this.gitRoot, true);
 
-		it("should remove --quiet parameter", async function(){
-			await gitCmd.init(this.gitRoot, true);
-
-			expect(gitCmd.cmd.calls.mostRecent().args[1]).not.toContain("--quiet");
-		});
+		expect(gitCmd.cmd.calls.mostRecent().args[1]).not.toContain("--quiet");
 	});
 
 	// describe("integration tests", function () {
